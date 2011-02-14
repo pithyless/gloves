803d88eaf9b9c48ffea8c45ce3f20e9a303455e6
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-02-14 17:03:25 +0100

    main.rb: using autogui

--- main.rb
+++ main.rb
@@ -1,5 +1,12 @@
 require_relative 'lib/toolkit'
 
-RUI::Application.init('hello') do
-  RUI::PushButton.new('Hello').show
+RUI::Application.init('hello') do |app|
+  widget = Qt::Widget.new
+  widget.gui = RUI::autogui do
+    layout(:type => :vertical) do
+      button(:name => :quit, :text => 'Quit')
+    end
+  end
+  widget.quit.on(:clicked) { app.exit }
+  widget.show
 end
