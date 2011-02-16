8705b8afea5f787a4f8454d09e6c8b4f286831f6
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-02-16 08:52:42 +0100

    RUI::GuiBuilder::Label image support moved to rui

--- lib/toolkit.rb
+++ lib/toolkit.rb
@@ -1,20 +1,6 @@
 $toolkit = :qt
 require 'rui'
 
-RUI::GuiBuilder::Label.class_eval do
-  def create_element(window, parent, desc)
-    label = Qt::Label.new(desc.opts[:text].to_s, window)
-    if desc.opts[:image]
-      label.setPixmap desc.opts[:image].to_pix
-    end
-    setup_widget(label, window, parent, desc)
-    if desc.opts[:buddy]
-      window.buddies[label] = desc.opts[:buddy]
-    end
-    label
-  end
-end
-
 class Qt::ScrollArea
   def add_widget(widget)
     setWidget(widget)
