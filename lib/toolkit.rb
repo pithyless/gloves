ed3232e4cae35959b4e622ce3794580fe20f07a0
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-02-18 14:49:29 +0100

    removes duplicate code

--- lib/toolkit.rb
+++ lib/toolkit.rb
@@ -21,20 +21,6 @@ class Qt::Image
   end
 end
 
-class Qt::ScrollArea
-  def add_widget(widget)
-    setWidget(widget)
-  end
-end
-
-module RUI::GuiBuilder
-  class ScrollArea < Widget
-    def factory(desc)
-      Qt::ScrollArea
-    end
-  end
-end
-
 module RUI::GuiBuilder
   class GraphicsPixmapItem < Widget
     def factory(desc)
